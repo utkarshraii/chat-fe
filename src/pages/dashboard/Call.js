16fb243b4d44f146a9b79b08682ad985edd5f5ae
utkarshraii <109386132+utkarshraii@users.noreply.github.com>
2023-08-26 16:00:46 +0530

    Update Call.js

--- src/pages/dashboard/Call.js
+++ src/pages/dashboard/Call.js
@@ -83,18 +83,18 @@ const Call = () => {
               </IconButton>
             </Stack>
             <Divider />
-            // <Stack
-            //   className="scrollHide"
-            //   sx={{ flexGrow: 1, overflowY: "scroll", height: "100%" }}
-            // >
-              // <SimpleBarStyle timeout={500} clickOnTrack={false}>
-              //   <Stack spacing={2.4}>
-              //     {CallList.map((el, idx) => {
-              //       return <CallLogElement key={idx} {...el} />;
-              //     })}
-              //   </Stack>
-              // </SimpleBarStyle>
-            // </Stack>
+                {/* <Stack
+              className="scrollHide"
+              sx={{ flexGrow: 1, overflowY: "scroll", height: "100%" }}
+            >
+              <SimpleBarStyle timeout={500} clickOnTrack={false}>
+                <Stack spacing={2.4}>
+                  {CallList.map((el, idx) => {
+                    return <CallLogElement key={idx} {...el} />;
+                  })}
+                </Stack>
+              </SimpleBarStyle>
+            </Stack>*/}
           </Stack>
         </Box>
       </Stack>
